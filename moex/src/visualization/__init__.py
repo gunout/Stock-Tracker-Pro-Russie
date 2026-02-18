0eb0361e1df7b167730bf6e834861f110f0372f7
Julien Talabaza <88831335+gunout@users.noreply.github.com>
2026-02-18 21:01:48 +0400

    Update __init__.py

--- moex/src/visualization/__init__.py
+++ moex/src/visualization/__init__.py
@@ -1,5 +1,6 @@
-"""Package de visualisation"""
+"""
+Package de visualisation
+"""
 from .charts import create_price_chart, create_candle_chart
-from .indicators import add_technical_indicators
 
-__all__ = ['create_price_chart', 'create_candle_chart', 'add_technical_indicators']
+__all__ = ['create_price_chart', 'create_candle_chart']
